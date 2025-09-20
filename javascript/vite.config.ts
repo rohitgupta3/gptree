0639b24baba702e2582974e2aaef8f829de51494
Rohit Gupta <gupta3@gmail.com>
2025-09-20 14:43:11 -0400

    mostly prettier change; key thing here is that assets should go to root for dev but to /static for prod; local dev behavior now normal, lets see if heroku works
    EDIT: looks like it still works on heroku

--- javascript/vite.config.ts
+++ javascript/vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   build: {
-    outDir: '../python/web/static',  // Build to backend's static folder
+    outDir: "../python/web/static", // Build to backend's static folder
     emptyOutDir: true,
   },
-  base: '/static/',  // Set base path for assets
-})
+  // Only use /static/ base in production builds
+  base: command === "build" ? "/static/" : "/",
+}));
